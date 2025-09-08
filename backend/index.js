e75da4f71c3e68dc0f99f6a74247e0cbfd4a95b8
Manish Kumar <mk7060655@gmail.com>
2025-09-08 20:05:34 +0530

    Fix frontend build path for deployment

--- backend/index.js
+++ backend/index.js
@@ -36,9 +36,10 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 // serve frontend
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
 app.listen(PORT, () => {
